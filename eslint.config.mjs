8ba6510636f59ca93c750f0b02b8ed4ab8270100
hyusap <paulayush@gmail.com>
2025-06-11 19:59:35 -0400

    🔧 update eslint configuration rules
    Added new ESLint rules to disable various perfectionist sorting options, enhancing code flexibility and reducing unnecessary constraints.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,4 +6,25 @@ import {fileURLToPath} from 'node:url'
 
 const gitignorePath = path.resolve(path.dirname(fileURLToPath(import.meta.url)), '.gitignore')
 
-export default [includeIgnoreFile(gitignorePath), ...oclif, prettier]
+export default [includeIgnoreFile(gitignorePath), ...oclif, prettier, {
+  rules: {
+    'perfectionist/sort-classes': 'off',
+    'perfectionist/sort-modules': 'off',
+    'perfectionist/sort-array-includes': 'off',
+    'perfectionist/sort-enums': 'off',
+    'perfectionist/sort-exports': 'off',
+    'perfectionist/sort-imports': 'off',
+    'perfectionist/sort-interfaces': 'off',
+    'perfectionist/sort-jsx-props': 'off',
+    'perfectionist/sort-keys': 'off',
+    'perfectionist/sort-maps': 'off',
+    'perfectionist/sort-named-exports': 'off',
+    'perfectionist/sort-named-imports': 'off',
+    'perfectionist/sort-object-types': 'off',
+    'perfectionist/sort-objects': 'off',
+    'perfectionist/sort-sets': 'off',
+    'perfectionist/sort-switch-case': 'off',
+    'perfectionist/sort-union-types': 'off',
+    'perfectionist/sort-variable-declarations': 'off',
+  }
+}]
